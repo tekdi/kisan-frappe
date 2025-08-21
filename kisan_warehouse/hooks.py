5495bee48321273b5fbbae1ad759fca42fb0baba
Vineet7875 <89721146+Vineet7875@users.noreply.github.com>
2025-08-21 20:40:26 +0530

    Email Notification to customer and owner on sauda save (#34)
    Co-authored-by: Vineet Jadhav <vineet.jadhav@tekditechnologies.com>

--- kisan_warehouse/hooks.py
+++ kisan_warehouse/hooks.py
@@ -4,7 +4,7 @@ app_publisher = "Deepak Patil"
 app_description = "KisanMitra Warehouse Management System"
 app_email = "deepak_p@tekditechnologies.com"
 app_license = "mit"
-email_account_email = "gafasox424@baxidy.com"
+
 # Export Enhanced fixtures
 fixtures = [
     # Core DocTypes
@@ -88,12 +88,6 @@ fixtures = [
     {
         "dt": "Notification",
         "filters": [["name", "in", ["Sauda Saved"]]]
-    },
-
-    # Email Accounts
-    {
-        "dt": "Email Account",
-        "filters": [["email_id", "in", [email_account_email]]]
     }
     
 ]
