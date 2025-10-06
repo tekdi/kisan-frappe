e93883a942344214d6279edefdad83b67a6ebfd5
Vineet7875 <89721146+Vineet7875@users.noreply.github.com>
2025-10-06 11:00:13 +0530

    Outward Jawak Implementation and Other feedback Changes (#77)
    Co-authored-by: Vineet Jadhav <vineet.jadhav@tekditechnologies.com>

--- kisan_warehouse/warehouse_rent/doctype/godown_floor/godown_floor.py
+++ kisan_warehouse/warehouse_rent/doctype/godown_floor/godown_floor.py
@@ -5,5 +5,5 @@ import frappe
 from frappe.model.document import Document
 
 
-class WarehouseFloor(Document):
+class GodownFloor(Document):
 	pass
--- kisan_warehouse/warehouse_rent/doctype/warehouse_floor/test_warehouse_floor.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Deepak Patil and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestWarehouseFloor(FrappeTestCase):
-	pass
